df1b0d9d1e535c64a3a3e8ed22595435cdf1f670
MWeberLambdaweb19 <mack.webb37@gmail.com>
2020-04-17 12:57:44 -0700

    Tests pass but horribly inefficient

--- adv.py
+++ adv.py
@@ -13,11 +13,11 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-map_file = "maps/test_line.txt"
+# map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-# map_file = "maps/main_maze.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -31,9 +31,104 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
-visited = []
+
 qq = Queue()
 st = Stack()
+
+st.push([player.current_room])
+qq.enqueue([player.current_room])
+visited = set()
+# print(st.stack)
+# test = st.pop()
+# print(test)
+while len(world.rooms) != len(visited):
+    path = st.pop()
+    vertex = path[-1]    
+    if vertex not in visited:
+        visited.add(vertex)
+        direction = random.choice(player.current_room.get_exits())
+        next_room = player.current_room.get_room_in_direction(direction)
+        if next_room in visited:
+            st.push([player.current_room])
+        else:
+            player.travel(direction)
+            traversal_path.append(direction)
+            st.push([player.current_room])
+    else:
+        direction = random.choice(vertex.get_exits())
+        player.travel(direction)
+        traversal_path.append(direction)
+        st.push([player.current_room])
+    # path_a = st.pop()
+    # path_b = qq.dequeue()
+    # vertex_a = path_a[-1]
+    # vertex_b = path_b[-1]
+    # if vertex_a not in visited:
+    #     visited.add(vertex_a)
+    #     direction = random.choice(player.current_room.get_exits())
+    #     next_room = player.current_room.get_room_in_direction(direction)
+    #     if next_room in visited:
+    #         st.push([player.current_room])
+    #     else:
+    #         player.travel(direction)
+    #         traversal_path.append(direction)
+    #         st.push([player.current_room])
+    # elif vertex_a in visited:
+    #     direction = random.choice(vertex_a.get_exits())
+    #     next_room = player.current_room.get_room_in_direction(direction)
+    #     if next_room in visited:
+    #         st.push([player.current_room])
+    #     else:
+    #         player.travel(direction)
+    #         traversal_path.append(direction)
+    #         st.push([player.current_room])
+
+    # if vertex_b not in visited:
+    #     visited.add(vertex_b)
+    #     direction = random.choice(player.current_room.get_exits())
+    #     next_room = player.current_room.get_room_in_direction(direction)
+    #     if next_room in visited:
+    #         qq.enqueue([player.current_room])
+    #     else:
+    #         player.travel(direction)
+    #         traversal_path.append(direction)
+    #         qq.enqueue([player.current_room])
+    # elif vertex_b in visited:
+    #     direction = random.choice(vertex_b.get_exits())
+    #     next_room = player.current_room.get_room_in_direction(direction)
+    #     if next_room in visited:
+    #         qq.enqueue([player.current_room])
+    #     else:
+    #         player.travel(direction)
+    #         traversal_path.append(direction)
+    #         qq.enqueue([player.current_room])
+    
+
+
+
+
+
+
+
+
+
+
+
+    # if vertex not in visited:
+    #     visited.add(vertex)
+    #     direction = random.choice(player.current_room.get_exits())
+    #     next_room = player.travel(direction)
+    #     if next_room in visited:
+    #         pass
+    #     else:
+    #         player.travel(direction)
+    #         traversal_path.append(direction)
+    #         st.push([player.current_room])
+    # else:
+    #     direction = random.choice(vertex.get_exits())
+    #     player.travel(direction)
+    #     traversal_path.append(direction)
+    #     st.push([player.current_room])
 
 # player_room = player.current_room
 # print('id',player_room.id)
@@ -45,17 +140,18 @@ st = Stack()
 # print(f'exits of room {player_room.id}', player_room.get_exits())
 # traversal_path.append(cat)
 
-qq.enqueue([player.current_room])
-while len(world.rooms) != len(visited):
-    path = qq.dequeue()
-    vertex = path[-1]
-    if vertex not in visited:
-        visited.append(vertex)
-        room = random.choice(vertex.get_exits())
-        if vertex.get_room_in_direction(room) not in visited:
-            player.travel(room)
-            traversal_path.append(player.travel(room))
-            print(player.current_room)
+# qq.enqueue([player.current_room])
+# st.push([player.current_room])
+# while len(world.rooms) != len(visited):
+#     path = qq.dequeue()
+#     vertex = path[-1]
+#     if vertex not in visited:
+#         visited.append(vertex)
+#         room = random.choice(vertex.get_exits())
+#         if vertex.get_room_in_direction(room) not in visited:
+#             player.travel(room)
+#             traversal_path.append(player.travel(room))
+#             print(player.current_room)
     # else:
     #     room = random.choice(vertex.get_exits())
     #     player.travel(room)
@@ -101,7 +197,7 @@ while len(world.rooms) != len(visited):
 # print(player.current_room.get_exits())
     
 
-print('t path', traversal_path)
+# print('t path', traversal_path)
 
 
 # TRAVERSAL TEST
