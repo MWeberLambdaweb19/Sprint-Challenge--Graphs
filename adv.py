154a1a693a945dc12c8ced87feefa576a60bbebe
MWeberLambdaweb19 <mack.webb37@gmail.com>
2020-04-17 11:01:00 -0700

    Lost on current project

--- adv.py
+++ adv.py
@@ -31,12 +31,77 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
-
+visited = []
 qq = Queue()
 st = Stack()
 
+# player_room = player.current_room
+# print('id',player_room.id)
+# print(f'exits of room {player_room.id}', player_room.get_exits())
+# cat = random.choice(player_room.get_exits())
+# player.travel(cat)
+# player_room = player.current_room
+# print('id',player_room.id)
+# print(f'exits of room {player_room.id}', player_room.get_exits())
+# traversal_path.append(cat)
 
-print(traversal_path)
+qq.enqueue([player.current_room])
+while len(world.rooms) != len(visited):
+    path = qq.dequeue()
+    vertex = path[-1]
+    if vertex not in visited:
+        visited.append(vertex)
+        room = random.choice(vertex.get_exits())
+        if vertex.get_room_in_direction(room) not in visited:
+            player.travel(room)
+            traversal_path.append(player.travel(room))
+            print(player.current_room)
+    # else:
+    #     room = random.choice(vertex.get_exits())
+    #     player.travel(room)
+    #     traversal_path.append(player.travel(room))
+    #     qq.enqueue([player.current_room])
+    
+# qq.enqueue([player.current_room])
+# curroom = player.current_room.get_exits()
+# visited = set()
+# while qq.size() > 0:
+#     path = qq.dequeue()
+#     vertex = path[-1]
+#     print('vertex',vertex)
+#     if vertex not in visited:
+#         visited.add(vertex)
+#         traversal_path.append(vertex)
+#         for exit_path in curroom:
+#             room = random.choice(player.current_room.get_exits())
+#             player.travel(room)
+#             new_path = list(path)
+#             new_path.append(exit_path)
+#             qq.enqueue(exit_path)
+
+# while qq.size() > 0:
+#     #build a dictionary of exits of which room is connected to each room
+#     #look for the most efficient path of each room
+#     print('current room', player.current_room)
+#     path = qq.dequeue()
+#     vertex = path[-1]
+#     print('vertex', vertex)
+#     if vertex not in visited:
+#         visited.add(vertex)
+#         for room_exit in player.current_room.get_exits():
+#             print('exit', room_exit)
+#             player.travel(room_exit)
+#             new_path = list(path)
+#             new_path.append(room_exit)
+#             qq.enqueue(room_exit)
+# traversed_rooms = []
+# traversed_rooms.append(player.current_room)
+
+# print(player.current_room)
+# print(player.current_room.get_exits())
+    
+
+print('t path', traversal_path)
 
 
 # TRAVERSAL TEST
